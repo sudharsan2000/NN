a6e29fe9b9e96557d4344f16f66c20ee9971c3ee
sudharsan2000 <40791737+sudharsan2000@users.noreply.github.com>
2019-07-12 10:49:12 +0530

    Add files via upload

--- NN2.py
+++ NN2.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_datasets("/tmp/data/",one_hot= "True")
+#from tensorflow.examples.tutorials.mnist import input_data
+#mnist = input_data.read_datasets("MNIST_data",one_hot= "True")
+import tensorflow.examples.tutorials.mnist.input_data as input_data
 
 n_nodes_hl1 = 500
 n_nodes_hl2 = 500
@@ -15,26 +15,51 @@ y= tf.placeholder('float')
 
 def neural_network_model(data):
 	hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1]) ),
-                          'biases',:tf.Variable(tf.random_normal(n_nodes_hl1))} #specify shape of the random normal
+                          'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))} #specify shape of the random normal
 	hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2]) ),
-                          'biases',:tf.Variable(tf.random_normal(n_nodes_hl2))}
+                          'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
 	hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3]) ),
-                          'biases',:tf.Variable(tf.random_normal(n_nodes_hl3))}
+                          'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
 	output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3,n_classes]) ),
-                          'biases',:tf.Variable(tf.random_normal(n_classes))}
+                        'biases':tf.Variable(tf.random_normal([n_classes]))}
 	# model : (data*weights) + biases
 
-	l1 = tf.add((data*hidden_1_layer['weights']) + hidden_1_layer['biases'])
+	l1 = tf.add((data*hidden_1_layer['weights']) , hidden_1_layer['biases'])
 	l1 = tf.nn.relu(l1) #rectified linear - activation fnc
 
-	l2 = tf.add((l1*hidden_2_layer['weights']) + hidden_2_layer['biases'])
+	l2 = tf.add((l1*hidden_2_layer['weights']) , hidden_2_layer['biases'])
 	l2 = tf.nn.relu(l2)
 
-	l3 = tf.add((l2*hidden_3_layer['weights']) + hidden_3_layer['biases'])
+	l3 = tf.add((l2*hidden_3_layer['weights']) , hidden_3_layer['biases'])
 	l3 = tf.nn.relu(l3)
 
 	output = (l3 * output_layer['weights'] ) + output_layer['biases']
 
 	return output
 
-	l
+def train_neural_network(x):
+        prediction = neural_network_model(x)
+        cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_ligits(prediction.y) )
+
+        optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+        hm_epochs = 1
+
+        with tff.Session() as sess:
+                sess.run(tf.initialize_all_variables())
+
+                for epoch in range(hm_epochs):
+                        epoch_loss = 0
+                        for _ in range(int (mnist.train.num_examples/batch_size)):
+                                epoch_x,epoch_y = mnist.train.next_batch(batch_size)
+                                _,c = sess.run([optimizer,cost], feed_dict = {x:epoch_x , y:epoch_y})
+                                epoch_loss+=c
+                        print('Epoch', epoch,'completed', ' loss: ',epoch_loss)
+
+                correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+                accuracy = tf.reduce_mean(tf.cast(correct,'float'))
+                print('Accuracy : ' ,accuracy.eval({x:mnist.test.images,y:mnist.test.labels}))
+
+
+
+train_neural_network(x)
